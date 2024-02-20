d29726d630c99abd9fa46bcda44f73bb73dfa642
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-02-19 23:16:03 -0800

    add models structure

---
+++ models.py
@@ -0,0 +1,63 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+
+
+app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+
+
+class Venue(db.Model):
+    __tablename__ = 'venues'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+
+class Artist(db.Model):
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+
+class Show(db.Model):
+    __tablename__ = 'shows'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    
+    artist = db.relationship('Artist', backref=db.backref('shows', cascade='all, delete'))
+    venue = db.relationship('Venue', backref=db.backref('shows', cascade='all, delete'))
+
+    def __init__(self, artist_id, venue_id, start_time):
+        self.artist_id = artist_id
+        self.venue_id = venue_id
+        self.start_time = start_time
+        self.update_status()
+    
+    def update_status(self):
+        if self.start_time < datetime.now():
+            self.past = True
+        else:
+            self.upcoming = True
